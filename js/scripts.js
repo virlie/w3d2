1d139db14e9a87b4f07dd768473b4e0d6c1a04a1
Cory West-Forbes and Joy P <joy@>
2018-03-27 11:02:29 -0700

    implement guessing logic and html response

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(function(){
-  $("#player1-submit").submit(function(event){
-    var playerOneText = $("#player1-text").val();
+  var playerOneText;
+  var playerTwoText;
+
+  $('#player1-submit').submit(function(event){
+    playerOneText = $('#player1-text').val();
 
     function removeVowels(str) {
       var newStrArray = [];
@@ -16,8 +19,26 @@ $(function(){
       return newStrArray.join('');
     }
 
+    $('#player2-guess').show();
     $('#form-hide').hide();
     $('#removeVowels').text(removeVowels(playerOneText));
+    event.preventDefault();
+  });
+
+  $('#player2-guess').submit(function(event) {
+    playerTwoText = $('#player2-text').val();
+
+    function compareStr(str1, str2) {
+      var responseStr;
+      if (str1 === str2) {
+        responseStr = 'Good Job';
+      } else {
+        responseStr = 'Try Again';
+      }
+      return responseStr;
+    }
+
+    $('#response').text(compareStr(playerOneText, playerTwoText));
     event.preventDefault();
   });
 });
